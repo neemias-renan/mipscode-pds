a7c24e95c89ff4fa01a31a313b27e9ca6ae60951
Neemias Renan <neemiasrenan2015@gmail.com>
2022-12-16 17:27:07 -0300

    criei a página de Login e Cadastro

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='mipscode/')),
     path('mipscode/', include('mipscode.urls')),
     path('admin/', admin.site.urls),
 ]
