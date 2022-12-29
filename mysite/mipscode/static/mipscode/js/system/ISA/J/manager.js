ef30d317e7534eda2fb7eed7b52de4e1c5555ccb
Neemias Renan <neemiasrenan2015@gmail.com>
2022-12-29 20:37:49 -0300

    remove all

--- mysite/mipscode/static/mipscode/js/system/ISA/J/manager.js
+++
@@ -1,7 +0,0 @@
-// import * as tools from "../toolkit.js";
-// import * as formatting from "./formatting.js";
-// import instructions from "./instructions.js";
-
-// export function isTypeJ(op) {
-//     return instructions[op] !== undefined;
-// }
